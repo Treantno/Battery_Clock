bc2b688db62e71964db1d0efd25635a1d395b49d
Lars Erik Svendsen <treant@moreless.net>
2023-06-07 00:07:29 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,10 @@ int latchPin = 0;
 int numDevices = 2;
 
 MAX72XX dispChain(latchPin, numDevices);
-MAX72XXDisplay  Disp_min(&dispChain, 8, 2);
-MAX72XXDisplay  Disp_sec(&dispChain, 10, 2);
+MAX72XXDisplay  Disp_min(&dispChain, 10, 2);
+MAX72XXDisplay  Disp_sec(&dispChain, 8, 2);
 MAX72XXDisplay  Disp2(&dispChain, 4, 4);
+MAX72XXDisplay  Disp3(&dispChain, 0, 4);
 
 unsigned long startTime; // to store the start time in milliseconds
 unsigned long startTime_zero; // to store the start time in milliseconds
@@ -27,6 +28,7 @@ void setup() {
   restart = 0;
 }
 
+#if 0
 int reversedigits(int number)
 {
     int reversed=0,multiply=0;
@@ -45,6 +47,7 @@ int reversedigits(int number)
 
     return reversed;
 }
+#endif
 
 void loop() {
   unsigned long elapsedTime = millis() - startTime; // calculate the elapsed time
@@ -59,13 +62,15 @@ void loop() {
     int seconds = remainingTime / 1000 % 60;
     int minutes = remainingTime / (1000 * 60) % 60;
 
-    Disp_min.writeNumber(reversedigits(minutes), MAX72XX::Character::ZERO);
-    Disp_sec.writeNumber(reversedigits(seconds), MAX72XX::Character::ZERO);
+//    Disp_min.writeNumber(reversedigits(minutes), MAX72XX::Character::ZERO);
+//    Disp_sec.writeNumber(reversedigits(seconds), MAX72XX::Character::ZERO);
+    Disp_min.writeNumber(minutes, MAX72XX::Character::ZERO);
+    Disp_sec.writeNumber(seconds, MAX72XX::Character::ZERO);
 
     if(half_second >= 500)
-      Disp_min.setDecimalPoint(1);
+      Disp_min.setDecimalPoint(0);
     else
-      Disp_min.setDecimalPoint(5);
+      Disp_min.setDecimalPoint(2);
   }
   else if(restart == 0)
   {
@@ -95,6 +100,7 @@ void loop() {
   }
 
   Disp2.writeNumber(cnt2, MAX72XX::Character::ZERO);
+  Disp3.writeNumber(cnt2 ^ 0xffff, MAX72XX::Character::ZERO);
  
   delay(100);
 }
